df271da1c1fd15ab6e9b138f3b2f39d795eceb22
Thejas <thejasganiga150503@gmail.com>
2025-06-02 22:25:36 +0530

    Print 1 To N

---
+++ Loops_Conditionals/printOneToN.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    printf("Enter the N : ");
+    cin >> N;
+    for (int i = 0; i <= N; i++)
+        cout << i << " ";
+    cout << endl;
+    return 0;
+}
