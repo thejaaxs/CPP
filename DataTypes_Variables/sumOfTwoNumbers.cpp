21d7879543544fcb351d92d3401dbc2ff6b8a605
Thejas <thejasganiga150503@gmail.com>
2025-06-10 22:56:31 +0530

    Sum Of 2 Numbers

---
+++ DataTypes_Variables/sumOfTwoNumbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter 2 Numbers : ";
+    cin >> a >> b;
+    std::cout << "The Sum of 2 Numbers is : " << a + b << std::endl;
+    return 0;
+}
